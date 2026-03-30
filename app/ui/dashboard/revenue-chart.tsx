2702f869ac5c91bb83a915eabafc48e00f7e9afc
Shtekata <gesheval@gmail.com>
2026-03-30 23:19:05 +0300

    revenue-chart.tsx changes

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -21,7 +21,7 @@ export default async function RevenueChart({ revenue }: { revenue: Revenue[] }) 
 
   return (
     <div className='w-full md:col-span-4'>
-      <h2 className={` mb-4 text-xl md:text-2xl`}>Recent Revenue</h2>
+      <h2 className='mb-4 text-xl md:text-2xl'>Recent Revenue</h2>
       <div className='rounded-xl bg-gray-50 p-4'>
         <div className='md:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4'>
           <div
@@ -32,7 +32,6 @@ export default async function RevenueChart({ revenue }: { revenue: Revenue[] }) 
               <p key={label}>{label}</p>
             ))}
           </div>
-
           {revenue.map(month => (
             <div key={month.month} className='flex flex-col items-center gap-2'>
               <div
